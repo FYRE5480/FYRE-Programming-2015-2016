f0443fe5cfdc1986c30f7c1dbcc8972d6afef1c1
Giselle Serate <Giselle Serate>
2016-02-06 13:54:14 -0800

    Added Arduino and Launchpad code that sends/receives counter; Mega slave, Launchpad master

---
+++ Giselle/Arduino stuff/Mega_slave/Mega_slave.ino
@@ -0,0 +1,40 @@
+// Wire Slave Sender
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Sends data as an I2C/TWI slave device
+// Refer to the "Wire Master Reader" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+int num = 0;
+  
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Mega has started program");
+  
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+
+}
+
+void loop() {
+//  Serial.println("Mega is in loop");
+  delay(100);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+//  Serial.println("I am writing");
+  Serial.println(num);
+//  Wire.write("I am sending ");
+  Wire.write(num); // respond with message of 8 bytes
+  // as expected by master
+  num++;
+}
