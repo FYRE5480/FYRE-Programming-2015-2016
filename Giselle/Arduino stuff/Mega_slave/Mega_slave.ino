431188b1a8758c229e917b67d249f53f03ea15fe
Giselle Serate <Giselle Serate>
2016-02-06 14:47:11 -0800

    Distance sensor is working; prints to serial monitor.

--- Giselle/Arduino stuff/Mega_slave/Mega_slave.ino
+++ Giselle/Arduino stuff/Mega_slave/Mega_slave.ino
@@ -13,28 +13,67 @@
 #include <Wire.h>
 
 int num = 0;
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+//#define LEDPin 13 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
   
 void setup() {
   Serial.begin(9600);
   Serial.println("Mega has started program");
   
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+//  Wire.begin(8);                // join i2c bus with address #8
+//  Wire.onRequest(requestEvent); // register event
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+//  pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 
 }
 
 void loop() {
-//  Serial.println("Mega is in loop");
-  delay(100);
+  //  Serial.println("Mega is in loop");
+  /* The following trigPin/echoPin cycle is used to determine the
+  distance of the nearest object by bouncing soundwaves off of it. */ 
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2); 
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = duration/58.2;
+  
+  if (distance >= maximumRange || distance <= minimumRange){
+  /* Send a negative number to computer and Turn LED ON 
+  to indicate "out of range" */
+  Serial.println("-1");
+//  digitalWrite(LEDPin, HIGH); 
+  }
+  else {
+  /* Send the distance to the computer using Serial protocol, and
+  turn LED OFF to indicate successful reading. */
+  Serial.println(distance);
+//  digitalWrite(LEDPin, LOW); 
+  }
+  
+  //Delay 50ms before next reading.
+  delay(500);
 }
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
-void requestEvent() {
-//  Serial.println("I am writing");
-  Serial.println(num);
-//  Wire.write("I am sending ");
-  Wire.write(num); // respond with message of 8 bytes
-  // as expected by master
-  num++;
-}
+//// function that executes whenever data is requested by master
+//// this function is registered as an event, see setup()
+//void requestEvent() {
+////  Serial.println("I am writing");
+//  Serial.println(num);
+////  Wire.write("I am sending ");
+//  Wire.write(num); // respond with message of 8 bytes
+//  // as expected by master
+//  num++;
+//}
