b2f6983e65f9cc130196de0dd4b3ffb91f156560
FYREStation <fyre5480@gmail.com>
2016-02-12 20:17:21 -0800

    I am sad. But I unbroke everything I broke tonight. It is time to go home and cry.

--- eclipseWorkspace/16.02.12 Cleaned And Stuff/src/Robot.cpp
+++ eclipseWorkspace/16.02.12 Cleaned And Stuff/src/Robot.cpp
@@ -15,13 +15,23 @@ bool isABool = 0;
 
 class Robot: public IterativeRobot
 {
+private:
 	I2C *i2cChannel2; // I ADDED THISSSSSS;KLADFSJKDSAMKSL
 	byte *anArray = new byte[1];
 	uint8_t *holderPtr = new uint8_t[1];
+	LiveWindow *lw = LiveWindow::GetInstance();
+	SendableChooser *chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
+	std::string autoSelected;
 
 	void RobotInit()
 	{
 		i2cChannel2 = new I2C(I2C::Port::kOnboard, I2C_SLAVE_ADR); // ;kadfslkmads;lkmdsa;lkmf;oewianm;oimeai;
+		chooser = new SendableChooser();
+		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
+		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
+		SmartDashboard::PutData("Auto Modes", chooser);
 	}
 
 	void AutonomousInit()
@@ -45,29 +55,14 @@ class Robot: public IterativeRobot
 	}
 
 	void ReadDistance() { // THIS IS MY FUNCTIONA;FLKDMS;LKM F;DLKMF;LAKDSM;FLKMAD;SLMF;oifkmlads;flkadsg;lkafdsm;km
-//		byte buff[TO_READ];
-//		i2cChannel2->Read(ADXL_REG, TO_READ, buff);
-//		SmartDashboard::PutNumber("i2c is", i2cChannel2->Read(ADXL_REG, TO_READ, buff));
-//		i2cChannel2->Write(ADXL_REG, 1);
-//		i2cChannel2->Read(ADXL_REG, TO_READ, buff);    // request 1 bytes from slave device #8
-//		long n = i2cChannel2 -> Read();
-//		i2cChannel2->Transaction(NULL, 0, anArray, TO_READ);
-//		i2cChannel2->Read(ADXL_REG, 1, holderPtr);
-//		i2cChannel2->ReadOnly(ADXL_REG, holderPtr);
 		i2cChannel2->Write(ADXL_REG, 1);
-//		holdLen = sizeof(holderPtr);
 		SmartDashboard::PutNumber("Connected via i2c?", i2cChannel2->AddressOnly());
-//		SmartDashboard::PutNumber("holdLen:", holdLen);
-//		SmartDashboard::PutNumber("sizeof(anArray):", sizeof(anArray));
 		SmartDashboard::PutNumber("Transaction return is a bool:", i2cChannel2->Transaction(NULL, 0, anArray, TO_READ));
-//		SmartDashboard::PutNumber("i2c Transaction return:", anArray[0]);
-//		SmartDashboard::PutNumber("i2c Read return:", holderPtr[0]);
-//		num++; // if putting 0 doesn't work
 	}
 
 	void TestPeriodic()
 	{
-
+		lw->Run();
 	}
 };
 
