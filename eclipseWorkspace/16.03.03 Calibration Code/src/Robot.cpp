7c29c8786b6bc2c38a59266675cb5cc3e2108c9b
Giselle Serate <Giselle Serate>
2016-03-04 14:47:46 -0800

    Added drive and shoot code to calibration. Did not test. Sorry.

--- eclipseWorkspace/16.03.03 Calibration Code/src/Robot.cpp
+++ eclipseWorkspace/16.03.03 Calibration Code/src/Robot.cpp
@@ -30,14 +30,66 @@ private:
 	float frontAngle;
 	float backAngle;
 
-	Joystick *shootStick;
+//	Joystick *shootStick;
 	float shooterY;
+
+	// YO SO WE STILL WANT TO SHOOT THIS ROBOT:
+	// Shooting motor controllers and joystick and limit switch
+	Jaguar *motor2, *motor3;
+	Talon *motor1; //Will be Talon: Victor for testing purposes only
+	Joystick *shootStick;
+	DigitalInput *shooterSwitch;
+
+	// Various drivy things and gyro and switch
+	CANTalon *frontLeftTalon, *rearLeftTalon, *rearRightTalon;
+	Victor *frontRightTalon;
+	RobotDrive *robotDrive;	// robot drive system
+	Joystick *flightStick;	// Flightstick
+	ADXRS450_Gyro *yawGyro;
+
+	int frontLeftChannel	 = 55;	// Channel Declarations
+		int rearLeftChannel	     = 1;
+		int frontRightChannel	 = 7;
+		int rearRightChannel	 = 2;
+
+		int flightstickChannel	 = 1;
+		int xboxChannel		     = 0;
+
+		int strafeButtonChannel  = 1;
+		int arcadeButtonChannel  = 2;
+		int fieldButtonChannel	 = 3;
+
+		int gyroResetChannel 	 = 8;
+
+		float deadZone			 = .2;
+
+		//	Drive variables
+		int driveMode = 0; // Default Drive Mode is Strafe
+		float flightX = 0;
+		float flightY = 0;
+		float flightZ = 0;
+		float flightThrottle = 0;
+
+	//Shooter variables
+		bool bPrime = false;
+		bool bPrimeOn = false;
+		int bPrimeRunning = 1;
+		bool bLoad = false;
+		bool bLoadOn = false;
+		int bLoadRunning = 1;
+		bool bHighGoalShoot = false;
+		float bHighGoal = 0;
+		bool bLowGoalShoot = false;
+		float bLowGoal = 0;
+		int shooterCounter = 0;
+		bool shootSwitch = 0;
+		int servoLocation = 0;
 
 	void RobotInit()
 	{
 		chooser = new SendableChooser();
-		chooser->AddDefault(camFront, (void*)&camDefault);
-		chooser->AddObject(camBack, (void*)&camCustom);
+		chooser->AddDefault(camFront, (void*)&camFront);
+		chooser->AddObject(camBack, (void*)&camBack);
 		SmartDashboard::PutData("Which camera are you testing?", chooser);
 
 //		Distance sensor declarations
@@ -48,6 +100,26 @@ private:
 		backCamServo = new Servo(11); // WHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT
 
 		shootStick = new Joystick(0);
+
+			// WE LIKE TO DRIVE, DON'T WE, STEVEN
+	//		Drive declarations
+			frontLeftTalon = new CANTalon(frontLeftChannel);
+			rearLeftTalon = new CANTalon(rearLeftChannel);
+			frontRightTalon = new Victor(frontRightChannel);
+			rearRightTalon = new CANTalon(rearRightChannel);
+			//frontLeftTalon->SetInverted(true);
+			rearLeftTalon->SetInverted(true);
+			yawGyro = new ADXRS450_Gyro();
+			robotDrive = new RobotDrive(frontLeftTalon, rearLeftTalon, frontRightTalon, rearRightTalon);
+			flightStick = new Joystick(flightstickChannel);
+			robotDrive -> SetSafetyEnabled(false);
+
+	//		Shooter variable declarations MAKE SURE YOU PLUG THEM INTO THE RIGHT PORTS WENDY
+			motor1 = new Talon(8); // loook above
+			motor2 = new Jaguar(4);
+			motor3 = new Jaguar(5);
+			shootStick = new Joystick(0);
+			shooterSwitch = new DigitalInput(0);
 	}
 
 	void AutonomousInit()
@@ -103,6 +175,156 @@ private:
 		SmartDashboard::PutNumber("Front angle:", frontAngle);
 		SmartDashboard::PutNumber("Back angle:", backAngle);
 	}
+
+//	PLEASE, STEVEN, WE'RE ALL COUNTING ON YOU
+	void DriveControl() { //Drives la wheels of the robot
+		flightX = flightStick->GetRawAxis(0); //Pull joystick side motion for later use
+		flightY = flightStick->GetRawAxis(1); //Pull joystick forward motion for later use (forward is -, backwards is +)
+		flightZ = flightStick->GetRawAxis(4); //Pull joystick twist motion for later use
+		flightThrottle = ((((shootStick->GetThrottle() - 1)*-1)/2) * .8 + .2); //Pull throttle to modify drive variables
+																		//Throttle value is between .2 and 1.0
+
+		if (fabs(flightX) < deadZone) { //Deaden x
+			flightX = 0;
+		}
+		if (fabs(flightY) < deadZone) { //Deaden y
+			flightY = 0;
+		}
+		if (fabs(flightZ) < deadZone) { //Deaden z
+			flightZ = 0;
+		}
+
+		if (flightStick->GetRawButton(strafeButtonChannel)){ //Set drive to strafe mode
+			driveMode = 0;
+		}
+		if (flightStick->GetRawButton(arcadeButtonChannel)){ //Set drive to arcade mode
+			driveMode = 1;
+		}
+		if (flightStick->GetRawButton(fieldButtonChannel)){ //Set drive to field-centric mode
+			driveMode = 2;
+		}
+		if (shootStick->GetRawButton(gyroResetChannel)){ //Reset gyro with the trigger
+			yawGyro->Reset();
+		}
+		flightX = flightX * flightThrottle;
+		flightY = flightY * flightThrottle;
+		flightZ = flightZ * flightThrottle;
+
+		if(driveMode == 1){
+			robotDrive->MecanumDrive_Cartesian(flightZ, flightY, flightX, 0);
+			SmartDashboard::PutString("DriveMode", "Arcade");
+		}
+		else if(driveMode == 2){
+			robotDrive->MecanumDrive_Cartesian(flightX, flightY, flightZ, yawGyro->GetAngle());
+			SmartDashboard::PutString("DriveMode", "Field");
+
+		}
+		else{
+			robotDrive->MecanumDrive_Cartesian(flightX, flightY, flightZ, 0);
+			SmartDashboard::PutString("DriveMode", "Strafe");
+
+		}
+
+
+		SmartDashboard::PutNumber("GyroAngle", yawGyro->GetAngle());
+	}
+
+	void PrimeMotors(){
+		bPrime = shootStick -> GetRawButton(9);
+		SmartDashboard::PutNumber("Load Running", bLoadRunning);
+		SmartDashboard::PutNumber("Prime Running", bPrimeRunning);
+		SmartDashboard::PutBoolean("Shooter Loaded", shooterSwitch);
+
+		if(bLoadRunning >= 0){ // bLoadRunning is -1 or 1 (1 means not running)
+			if(bPrime == true){ // if we press the button
+				if(bPrimeOn == false){ // if the button is not pressed last iteration
+					bPrimeOn = true; // say button was pressed
+					bPrimeRunning = -bPrimeRunning; // turn prime on or off
+				}
+			}
+			else{
+				bPrimeOn = false; // when you let go, say the button was let go of
+			}
+
+		}
+
+
+		if(bPrimeRunning <= 0){
+			motor2 -> Set(1); // turn it on
+			motor3 -> Set(-1);
+			flightStick -> SetRumble(Joystick::kRightRumble, 1);
+		}
+		else{
+			motor2 -> Set(0); // turn it off
+			motor3 -> Set(0);
+			flightStick -> SetRumble(Joystick::kRightRumble, 0);
+		}
+	}
+
+	void Load() { // See prime motors for logicy things
+		bLoad = shootStick -> GetRawButton(11);
+		if(bPrimeRunning >= 0){
+			if (bLoad == true) {
+				if (bLoadOn == false) { // boolean for whether button is "on" or not
+					bLoadOn = true;
+					bLoadRunning = -bLoadRunning; // boolean for whether motor is running or not
+				}
+			}
+			else {
+				bLoadOn = false;
+			}
+		}
+
+		if (bLowGoalShoot == false && bHighGoalShoot == false){ // make sure that it is not trying to shoot
+			if (bLoadRunning <= 0){
+				motor1 -> Set(.8);
+				flightStick -> SetRumble(Joystick::kLeftRumble, 1);
+			}
+			else{
+				motor1 -> Set(0);
+				flightStick -> SetRumble(Joystick::kLeftRumble, 0);
+			}
+		}
+	}
+
+	void HighGoal(){ // shoots for a time period -- is not a toggle
+		bHighGoal = flightStick -> GetRawAxis(3);
+
+		if(bLowGoalShoot == false && bLoadRunning >= 0){
+			if(bHighGoal > .6){
+				if(shooterCounter < 4){
+					motor1 -> Set(-.5);
+				}
+				else{
+					motor1 -> Set(.5);
+				}
+				shooterCounter++;
+				bHighGoalShoot = true;
+			}
+			else{
+				motor1 -> Set(0);
+				bHighGoalShoot = false;
+				shooterCounter = 0;
+			}
+
+		}
+	}
+
+	void LowGoal(){ // See HighGoal
+				bLowGoal = flightStick -> GetRawAxis(2); // gets left trigger
+
+				if(bHighGoalShoot == false && bLoadRunning >= 0){
+					if(bLowGoal > .6){
+						motor1 -> Set(-1);
+						bLowGoalShoot = true;
+					}
+					else{
+						motor1 -> Set(0);
+						bLowGoalShoot = false;
+					}
+
+				}
+			}
 
 	void TestPeriodic()
 	{
